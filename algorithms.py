b12250de3fa35f2c4ca663f5fec0b6144d1a6264
Charles Ross <chivalry@mac.com>
2020-04-13 14:08:35 -0700

    Add is_prime

--- algorithms.py
+++ algorithms.py
@@ -24,3 +24,26 @@ def is_anagram(word, candidate):
     normalized_word = sorted(tuple(word.lower()))
     normalized_candidate = sorted(tuple(candidate.lower()))
     return normalized_word == normalized_candidate and word.lower() != candidate.lower()
+
+
+def is_prime(num: int) -> bool:
+    """Uses early prime facts to determine primacy more quickly."""
+    if not isinstance(num, int) or num <= 0:
+        raise ValueError('Error: Only positive integers can be prime')
+    if num in [2, 3, 5, 7]:
+        return True
+    if num < 2 or num % 2 == 0 or num == 9:
+        return False
+    if num % 3 == 0:
+        return False
+    root = int(num ** 0.5)
+    fact = 5
+    while fact <= root:
+        if num % fact == 0 or num % (fact + 2) == 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    for i in range(1, 26):
+        print(i, is_prime(i))
